f568d8e2da5f3dbddd86499cd337f8d61f97085e
tongjames0923 <tongjames@live.com>
2022-10-31 10:38:12 +0800

    统一api的规范 新增答案和考试权限控制器 考试服务完成

--- API_Server/src/main/java/tbs/api_server/backend/serviceImp/ExamImp.java
+++ API_Server/src/main/java/tbs/api_server/backend/serviceImp/ExamImp.java
@@ -30,49 +30,50 @@ public class ExamImp implements ExamService
     public ServiceResult getExamByStatus(int status, int from, int num) throws BackendError
     {
         List<ExamInfo> list = mp.getExamsByStatus(status, from, num);
-        if(list!=null&&list.size()>0)
+        if (list != null && list.size() > 0)
         {
-            return ServiceResult.makeResult(SUCCESS,list);
+            return ServiceResult.makeResult(SUCCESS, list);
         }
-        throw _ERROR.throwError(EC_DB_SELECT_NOTHING,"不存在此状态的考试",status);
+        throw _ERROR.throwError(EC_DB_SELECT_NOTHING, "不存在此状态的考试", status);
     }
 
     @Override
     public ServiceResult getExamByName(String name) throws BackendError
     {
         ExamInfo examInfo = mp.getExamIDByExamName(name);
-        if(examInfo!=null)
+        if (examInfo != null)
         {
-            return ServiceResult.makeResult(SUCCESS,examInfo);
+            return ServiceResult.makeResult(SUCCESS, examInfo);
         }
-        throw _ERROR.throwError(EC_DB_SELECT_NOTHING,"不存在此名称的考试",name);
+        throw _ERROR.throwError(EC_DB_SELECT_NOTHING, "不存在此名称的考试", name);
     }
+
     @Override
-    public ServiceResult uploadExam(int user,String name, Date beg, String note, Integer length, byte[] file) throws BackendError
+    public ServiceResult uploadExam(int user, String name, Date beg, String note, Integer length, byte[] file)
+            throws BackendError
     {
-        int c= mp.uploadExam(name,beg,note,file,length);
-        if(c<=0)
-        throw _ERROR.throwError(EC_DB_INSERT_FAIL,"上传考试失败",null);
-        ExamInfo info= mp.getExamIDByExamName(name);
-        c= permit.putPermission(user,info.getExam_id(),1,1,1);
-        if(c<=0)
-            throw _ERROR.throwError(EC_DB_INSERT_FAIL,"考试权限除市场赋予失败");
-        return ServiceResult.makeResult(SUCCESS,null);
+        int c = mp.uploadExam(name, beg, note, file, length);
+        if (c <= 0)
+            throw _ERROR.throwError(EC_DB_INSERT_FAIL, "上传考试失败", null);
+        ExamInfo info = mp.getExamIDByExamName(name);
+        c = permit.putPermission(user, info.getExam_id(), 1, 1, 1);
+        if (c <= 0)
+            throw _ERROR.throwError(EC_DB_INSERT_FAIL, "考试权限除市场赋予失败");
+        return ServiceResult.makeResult(SUCCESS, null);
     }
 
     @Override
     public ServiceResult deleteExam(int id, int userid) throws BackendError
     {
-        if(needWrite(userid,id))
+        if (needWrite(userid, id))
         {
-           int c= mp.deleteExam(id);
-           if(c>0)
-               return ServiceResult.makeResult(SUCCESS);
-           throw _ERROR.throwError(EC_DB_DELETE_FAIL,"删除考试失败");
-        }
-        else
+            int c = mp.deleteExam(id);
+            if (c > 0)
+                return ServiceResult.makeResult(SUCCESS);
+            throw _ERROR.throwError(EC_DB_DELETE_FAIL, "删除考试失败");
+        } else
         {
-            throw _ERROR.throwError(EC_LOW_PERMISSIONS,"权限不足，不可删除考试");
+            throw _ERROR.throwError(EC_LOW_PERMISSIONS, "权限不足，不可删除考试");
         }
     }
 
@@ -80,61 +81,62 @@ public class ExamImp implements ExamService
     public ServiceResult list(int from, int num) throws BackendError
     {
         List<ExamInfo> list = mp.list(from, num);
-        if(list.size() > 0)
-            return ServiceResult.makeResult(SUCCESS,list);
-        throw _ERROR.throwError(EC_DB_SELECT_NOTHING,"不存在考试");
+        if (list.size() > 0)
+            return ServiceResult.makeResult(SUCCESS, list);
+        throw _ERROR.throwError(EC_DB_SELECT_NOTHING, "不存在考试");
 
     }
 
-    private boolean needCheck(int user,int examid)
+    private boolean needCheck(int user, int examid)
     {
-        ExamPermission permission= permit.getPermission(user,examid);
-        if(permission==null)
+        ExamPermission permission = permit.getPermission(user, examid);
+        if (permission == null)
             return false;
-        return permission.getWritealbe()!=0||permission.getCheckable()!=0;
+        return permission.getWritealbe() != 0 || permission.getCheckable() != 0;
     }
-    private boolean needWrite(int user,int examid)
+
+    private boolean needWrite(int user, int examid)
     {
-        ExamPermission permission= permit.getPermission(user,examid);
-        if(permission==null)
+        ExamPermission permission = permit.getPermission(user, examid);
+        if (permission == null)
             return false;
-        return permission.getWritealbe()!=0;
+        return permission.getWritealbe() != 0;
     }
 
     @Override
-    public ServiceResult updateStatus(int status,int examid) throws BackendError
+    public ServiceResult updateStatus(int status, int examid) throws BackendError
     {
-        int c= mp.updateExam(examid, const_Exam.col_status,status);
-        if(c>0)
+        int c = mp.updateExam(examid, const_Exam.col_status, status);
+        if (c > 0)
             return ServiceResult.makeResult(SUCCESS);
-        throw _ERROR.throwError(EC_DB_UPDATE_FAIL,"更新状态失败");
+        throw _ERROR.throwError(EC_DB_UPDATE_FAIL, "更新状态失败");
     }
 
     @Override
     public ServiceResult updateLen(int len, int user, int examid) throws BackendError
     {
-        int c= mp.updateExam(examid, const_Exam.col_len,len);
-        if(c>0)
+        int c = mp.updateExam(examid, const_Exam.col_len, len);
+        if (c > 0)
             return ServiceResult.makeResult(SUCCESS);
-        throw _ERROR.throwError(EC_DB_UPDATE_FAIL,"更新状态失败");
+        throw _ERROR.throwError(EC_DB_UPDATE_FAIL, "更新状态失败");
     }
 
     @Override
     public ServiceResult updateNote(String note, int user, int examid) throws BackendError
     {
-        int c= mp.updateExam(examid, const_Exam.col_notes,note);
-        if(c>0)
+        int c = mp.updateExam(examid, const_Exam.col_notes, note);
+        if (c > 0)
             return ServiceResult.makeResult(SUCCESS);
-        throw _ERROR.throwError(EC_DB_UPDATE_FAIL,"更新状态失败");
+        throw _ERROR.throwError(EC_DB_UPDATE_FAIL, "更新状态失败");
     }
 
     @Override
     public ServiceResult updateBegin(Date date, int user, int examid) throws BackendError
     {
-        int c= mp.updateExam(examid, const_Exam.col_begin,date);
-        if(c>0)
+        int c = mp.updateExam(examid, const_Exam.col_begin, date);
+        if (c > 0)
             return ServiceResult.makeResult(SUCCESS);
-        throw _ERROR.throwError(EC_DB_UPDATE_FAIL,"更新状态失败");
+        throw _ERROR.throwError(EC_DB_UPDATE_FAIL, "更新状态失败");
     }
 
     @Override
@@ -142,16 +144,16 @@ public class ExamImp implements ExamService
     {
         try
         {
-            int c= mp.updateExam(examid, const_Exam.col_name,name);
-            if(c>0)
+            int c = mp.updateExam(examid, const_Exam.col_name, name);
+            if (c > 0)
                 return ServiceResult.makeResult(SUCCESS);
-            throw _ERROR.throwError(EC_DB_UPDATE_FAIL,"更新状态失败");
-        }catch (DuplicateKeyException ec)
+            throw _ERROR.throwError(EC_DB_UPDATE_FAIL, "更新状态失败");
+        } catch (DuplicateKeyException ec)
         {
-           throw  _ERROR.throwError(FC_DUPLICATE,"重复考试名称");
-        }catch (Exception e)
+            throw _ERROR.throwError(FC_DUPLICATE, "重复考试名称");
+        } catch (Exception e)
         {
-            throw _ERROR.throwError(EC_UNKNOWN,e.getMessage(),e);
+            throw _ERROR.throwError(EC_UNKNOWN, e.getMessage(), e);
         }
 
     }
